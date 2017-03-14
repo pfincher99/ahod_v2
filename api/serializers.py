459e3dfcb571d423a86fcee0d8fe5467b0343e29
Pete Fincher <pfincher@cisco.com>
2017-03-14 18:41:58 -0400

    First

--- api/serializers.py
+++ api/serializers.py
@@ -2,24 +2,6 @@ from rest_framework import serializers
 from api.models import Container
 
 class ContainerSerializer(serializers.HyperlinkedModelSerializer):
-    #id = serializers.IntegerField()
-
     class Meta:
         model = Container
         fields = ('container_id', 'ip_address', 'passwd_hash', 'status', 'timestamp')
-       
-   
-
-#class DatapointSerializer(serializers.HyperlinkedModelSerializer):
-#    device = DeviceSerializer()
-#
-#    class Meta:
-#        model = Datapoint
-#        fields = ('device', 'monitor','devicetype')
-#
-#    def create(self, validated_data):
-#        device = validated_data.pop('device')
-#        print device
-#        device = Device.objects.get_or_create(name=device['name'], note=device['name'])[0]
-#        dp = Datapoint.objects.create(device=device, **validated_data)
-#        return dp
