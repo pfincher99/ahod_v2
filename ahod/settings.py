47b67a05373eb475959cb45a9433ddc825abdaad
Pete Fincher <pfincher@cisco.com>
2017-03-24 23:52:10 -0400

    debug set to False

--- ahod/settings.py
+++ ahod/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1kbctf6e940=$!-stic%k#1ysb41iwr(w577!ep20#kt5^5!n0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
