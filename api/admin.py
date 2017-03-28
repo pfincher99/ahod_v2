3a5b05303099af0e464852f8509bd806940fad19
Pete Fincher <pfincher@cisco.com>
2017-03-27 21:57:12 -0400

    added Plc to admin view

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from api.models import Container
+from api.models import Plc
 # Register your models here.
 admin.site.register(Container)
-
+admin.site.register(Plc)
